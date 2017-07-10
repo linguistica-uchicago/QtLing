20411c924b658b4d594d6eae24c9613ea4e71ad4
John Goldsmith <johngoldsmith@techstaffs-iMac.local>
2017-07-10 17:01:08 -0500

    A few changes in mainwindow; reads in browncorpus, does analysis and posts to GUI table.

--- QtLing/mainwindow.cpp
+++ QtLing/mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "iostream"
 #include <QStandardItemModel>
 #include <QList>
 #include <QFile>
 #include <QTextStream>
 #include <QDebug>
 #include <stdio.h>
+#include <QDir>
 #include "WordCollection.h"
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -21,33 +23,35 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::on_pushButton_clicked()
-{
-//    FILE* pFile = fopen("test.txt", "r");
-//    if (!pFile) {
-//        qDebug() << "rippp";
-//        return;
-//    }
-//    char buff[100];
+{   QString abspath("/Users/johngoldsmith/projects/QtLing/QtLing/QtLing");
+    if (!QDir::setCurrent(abspath))
+    {
+        qDebug() << "Could not change to home directory";
+    }
+    QString filename = "./browncorpus.dx1";
+    QFile file(filename);
+    if (! file.exists()){
+        qDebug() << filename << "File does not exist.";
+    }else
+    {
+        qDebug() << "File exists at least.";
+    }
+//    file.open(QFile::Text);
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        qDebug() << "rippp";
+        return;
+    }
+    QTextStream in(&file);
+    while (!in.atEnd())
+    {
+        QString line = in.readLine();
+        line.simplified();
+        QStringList words = line.split (" ");
+        QString word = words[0];
+        Words[word] = 1;
 
-//    while(!feof(pFile)) {
-//        if (fgets(buff, 100, pFile) == NULL) break;
-//    }
-//    fclose(pFile);
-//    char* buffer = buff;
-//    QString content(buffer);
+    }
 
-//    QString filename = QString("test.txt");
-//    QFile infile(filename);
-
-//    if (!infile.open(pFile, QIODevice::ReadOnly)) {
-//        qDebug() << "well shit.";
-//        return;
-//    }
-
-//    QString content = infile.readAll();
-
-//    QList<QString> list = content.split(" ");
-    //int i;
 
 //    CWordCollection wordCollection;
 
@@ -58,17 +62,21 @@ void MainWindow::on_pushButton_clicked()
 //        qDebug() << list.at(i);
 //        wordCollection.operator <<( list.at(i) );
 //    }
-////    qSort(wordCollection.m_WordList);
+//     qSort(wordCollection.m_WordList);
 
-//    QStandardItemModel *model = new QStandardItemModel(list.count(),1,this); //n Rows and 3 Columns
-//    model->setHorizontalHeaderItem(0, new QStandardItem(QString("Column1 Header")));
-//    //    model->setHorizontalHeaderItem(1, new QStandardItem(QString("Column2 Header")));
-//    //    model->setHorizontalHeaderItem(2, new QStandardItem(QString("Column3 Header")));
+    QMapIterator<QString,int> iter(Words);
 
-//    ui->tableView->setModel(model);
-//    int len = list.count();
-//    for (i = 0; i < len; i++) {
-//        QStandardItem *ithRow = new QStandardItem(wordCollection.m_WordList.at(i));
-//        model->setItem(i,0,ithRow);
-//    }
+    QStandardItemModel *model = new QStandardItemModel(Words.size(),1,this); //n Rows and 3 Columns
+    model->setHorizontalHeaderItem(0, new QStandardItem(QString("Column1 Header")));
+    model->setHorizontalHeaderItem(1, new QStandardItem(QString("Column2 Header")));
+    model->setHorizontalHeaderItem(2, new QStandardItem(QString("Column3 Header")));
+
+    ui->tableView->setModel(model);
+    int rowno = 0;
+    while (iter.hasNext()) {
+        iter.next();
+        QStandardItem *ithRow = new QStandardItem(iter.key());
+        model->setItem(rowno,0,ithRow);
+        rowno++;
+    }
 }
